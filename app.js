2ce1c5fd361d22c25904bcf0272bb5e0d96d99a2
Anthony GODART <anthonygodart@mbp-de-anthony.home>
2024-09-05 12:11:18 +0200

    added install button

--- app.js
+++ app.js
@@ -9,3 +9,31 @@ if ('serviceWorker' in navigator) {
             });
     });
 };
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Empêcher l'affichage immédiat de la boîte de dialogue
+    e.preventDefault();
+    // Sauvegarder l'événement pour pouvoir l'utiliser plus tard
+    deferredPrompt = e;
+    // Afficher le bouton d'installation personnalisé
+    const installButton = document.getElementById('installButton');
+    installButton.style.display = 'block';
+
+    installButton.addEventListener('click', () => {
+        // Cacher le bouton d'installation
+        installButton.style.display = 'none';
+        // Afficher la boîte de dialogue d'installation
+        deferredPrompt.prompt();
+        // Attendre que l'utilisateur réponde à la boîte de dialogue
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('L\'utilisateur a accepté l\'installation');
+            } else {
+                console.log('L\'utilisateur a refusé l\'installation');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
